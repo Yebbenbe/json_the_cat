a2305c6d4a6645d4418aa47639c34f9df1615706
Yebbenbe <lmtpox@gmail.com>
2023-10-23 03:01:29 -0300

    initial commit

---
+++ breedFetcher.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const breedName = process.argv[2];
+
+// api url with breed as query param
+const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+// sending request to api url
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    onst data = JSON.parse(body);
+    console.log('Parsed Response Data:', data);
+  }
+});
