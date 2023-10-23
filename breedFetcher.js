47f50f5915aca856b8457532a228d12ff949baac
Yebbenbe <lmtpox@gmail.com>
2023-10-23 04:03:38 -0300

    refactors breedFetcher to be called by index.js. Adds mocha/chai test directory and tests.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,24 @@
 const request = require('request');
-const breedName = process.argv[2];
-
-// api url with breed as query param
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 // sending request to api url
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    console.log('Parsed Response Data:', data);
-  }
-});
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      
+      // if no data is returned
+      if (data.length === 0) {
+        callback('Breed not found', null);
+
+      } else {
+        callback(null, data[0].description);
+      }
+    
+    }
+  
+  });
+};
+
+module.exports = {fetchBreedDescription};
